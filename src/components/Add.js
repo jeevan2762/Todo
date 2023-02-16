3eb13f9d2db149853cbdabe0e3e44555654bd347
jeevan kumar <jjjeeevvvaaan@gmail.com>
2023-02-16 18:03:19 +0530

    Installed Mui icons.

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,5 +1,6 @@
 import { Button } from '@mui/material';
 import React from 'react';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 
 const Add = (context) => {
 	const { state: { initialText, list }, state, setState } = context;
@@ -9,6 +10,7 @@ const Add = (context) => {
 			className="addButton"
 			variant="outlined"
 			size="small"
+			startIcon={ <AddOutlinedIcon/> }
 			onClick={ () => setState({
 				...state,
 				list: [...list, { name: initialText }],
